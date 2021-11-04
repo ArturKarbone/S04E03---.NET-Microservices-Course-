698dd96b5f886bc3619611659498e440900c514c
Artur Karbone <artur.karbone@topia.com>
2021-11-04 21:40:48 +0200

    introduce CustomMiddleware to track hostames

---
+++ PlatformService/Middleware/CustomHeaders.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace PlatformService.Middleware
+{
+    public class CustomHeaders
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomHeaders(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+            httpContext.Response.Headers.Add("hostname", Environment.MachineName);
+
+            return _next(httpContext);
+        }
+    }
+}
--- PlatformService/Startup.cs
+++ PlatformService/Startup.cs
@@ -1,21 +1,16 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using PlatformService.AsyncDataServices;
 using PlatformService.Data;
+using PlatformService.Middleware;
 using PlatformService.SyncDataServices.Grpc;
 using PlatformService.SyncDataServices.Http;
 
@@ -79,6 +74,7 @@ namespace PlatformService
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseMiddleware<CustomHeaders>();
 
             app.UseEndpoints(endpoints =>
             {
